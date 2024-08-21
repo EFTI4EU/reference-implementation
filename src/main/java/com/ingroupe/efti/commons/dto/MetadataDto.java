504563d357c0bfe719f89fd239c58edd6991ca01
osouley <ousmane.souley@ext.ingroupe.com>
2024-08-21 16:57:13 +0200

    EFTI-970-971 fix vehicle id and isdangerousgood parsing for gate to gate metadata request

--- src/main/java/com/ingroupe/efti/commons/dto/MetadataDto.java
+++ src/main/java/com/ingroupe/efti/commons/dto/MetadataDto.java
@@ -24,7 +24,7 @@ public class MetadataDto extends AbstractUilDto {
     private long id;
     @JsonProperty("isDangerousGoods")
     @XmlElement(name = "isDangerousGoods")
-    private boolean isDangerousGoods;
+    private Boolean isDangerousGoods;
     private String journeyStart;
     private String countryStart;
     private String journeyEnd;
