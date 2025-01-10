2da30808405ee68aac711aa52b3554ab6504bb13
SOULEY Ousmane <ousmane.souley@ext.ingroupe.com>
2025-01-10 12:03:41 +0000

    resolved some sonar issues, fixed sonar exclusions and used dependency...

--- platform-gate-simulator/src/test/java/eu/efti/platformgatesimulator/service/AbstractTest.java
+++ platform-gate-simulator/src/test/java/eu/efti/platformgatesimulator/service/AbstractTest.java
@@ -5,14 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.cfg.CoercionAction;
 import com.fasterxml.jackson.databind.cfg.CoercionInputShape;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.jakarta.xmlbind.JakartaXmlBindAnnotationModule;
 import eu.efti.commons.utils.SerializeUtils;
 
 public abstract class AbstractTest {
 
-    public final SerializeUtils serializeUtils = new SerializeUtils(objectMapper(), initMapper());
+    public final SerializeUtils serializeUtils = new SerializeUtils(objectMapper());
 
     public ObjectMapper objectMapper() {
         final ObjectMapper objectMapper = new ObjectMapper();
@@ -22,14 +20,5 @@ public abstract class AbstractTest {
         objectMapper.coercionConfigDefaults().setCoercion(CoercionInputShape.String, CoercionAction.AsEmpty)
                 .setCoercion(CoercionInputShape.EmptyString, CoercionAction.AsEmpty);
         return objectMapper;
-    }
-
-    private XmlMapper initMapper() {
-        final XmlMapper mapper = new XmlMapper();
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        mapper.registerModule(new JavaTimeModule());
-        mapper.registerModule(new JakartaXmlBindAnnotationModule());
-        return mapper;
     }
 }
