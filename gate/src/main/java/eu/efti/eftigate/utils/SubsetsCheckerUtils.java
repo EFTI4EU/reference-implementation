0b0b4eb7c9a47d4aba12fed795164994e1b26212
Julien Thueux <julien.thueux@ingroupe.com>
2025-02-06 15:04:41 +0100

    refactor(develop) : add log foggoten on branch subset-filter

--- gate/src/main/java/eu/efti/eftigate/utils/SubsetsCheckerUtils.java
+++ gate/src/main/java/eu/efti/eftigate/utils/SubsetsCheckerUtils.java
@@ -2,10 +2,12 @@ package eu.efti.eftigate.utils;
 
 import eu.efti.v1.edelivery.SubsetIds;
 import lombok.experimental.UtilityClass;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.List;
 
 @UtilityClass
+@Log4j2
 public class SubsetsCheckerUtils {
 
     public static final String FULL = "full";
@@ -17,6 +19,7 @@ public class SubsetsCheckerUtils {
         try {
             return subsets.stream().allMatch(subset -> subset.equals(SubsetIds.fromValue(subset).value()));
         } catch (IllegalArgumentException e) {
+            log.error("Error bad subsets", e);
             return false;
         }
     }
