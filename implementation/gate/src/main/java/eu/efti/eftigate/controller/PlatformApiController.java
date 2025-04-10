ec5a10c44b78500805f5e3b9255c5c2ac50dc9ff
Mikko Suniala <mikko.suniala@solita.fi>
2025-04-10 14:54:47 +0300

    EREF-27 allow access to gate's platform REST api via pre-auth headers
    Gate expects to get headers that identify the calling client and its
    role. These headers must be added by some other component, that is
    responsible for authenticating the client. This component may be,
    for example, a proxy like nginx.
    
    For local development, configure the Apache proxy to accept headers
    X-Mock-Pre-Authenticated-User-Id and X-Mock-Pre-Authenticated-User-Role
    that may be used to authenticate the client.

--- implementation/gate/src/main/java/eu/efti/eftigate/controller/PlatformApiController.java
+++ implementation/gate/src/main/java/eu/efti/eftigate/controller/PlatformApiController.java
@@ -2,22 +2,19 @@ package eu.efti.eftigate.controller;
 
 import eu.efti.eftigate.controller.api.platform.V0Api;
 import eu.efti.eftigate.dto.GetWhoami200Response;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/platform")
 @Tag(name = "Platform API", description = "REST API for the platforms")
 public class PlatformApiController implements V0Api {
-
     @Override
     public ResponseEntity<GetWhoami200Response> getWhoami() {
-        return null;
+        var ctx = PlatformApiContextResolver.getPlatformContextOrFail();
+        return ResponseEntity.ok(new GetWhoami200Response(ctx.platformId(), ctx.role()));
     }
 
     @Override
